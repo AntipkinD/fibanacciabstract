774b243839ee96b31b63615396eb2e881497e706
mushroomvoodoo <normeeboy@gmail.com>
2024-02-15 10:10:35 +0300

    Добавьте файлы проекта.

---
+++ fibanacciabstract/IFibanacci.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibanacciabstract
+{
+    public interface IFibanacci
+    {
+        internal static void Fibanacci<T>(dynamic n)
+        {
+            if (n == 0) Console.WriteLine(0);
+            dynamic prev = 0;
+            Console.Write(prev + " ");
+            dynamic next = 1;
+            Console.Write(next + " ");
+            for (int i = 2; i < n; i++)
+            {
+                dynamic sum = prev + next;
+                prev = next;
+                next = sum;
+                Console.Write(next + " ");
+            }
+        }
+    }
+}
---
+++ fibanacciabstract/Program.cs
@@ -0,0 +1,12 @@
+﻿using fibanacciabstract;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите количество чисел в ряду: ");
+        IFibanacci.Fibanacci<dynamic>(Convert.ToInt64(Console.ReadLine()));
+        
+    }
+}
